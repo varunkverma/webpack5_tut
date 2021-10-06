c2d50975181dd3f6de5b0b9f8ddf9b42c200dc21
Varun <varunkverma1996@gmail.com>
2021-10-06 12:53:01 +0530

    respos2

---
+++ webpack-tutorial-integrating-express-js-into-a-single-page-application/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+// serving static files
+app.use("/static", express.static(path.resolve(__dirname, "../dist")));
+
+app.get("/", (req, res) => {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/index.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contentFromHtmlFile);
+});
+
+app.listen(3000, () => {
+  console.log("application is listening on port: 3000");
+});
