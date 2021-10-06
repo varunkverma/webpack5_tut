c79133ee43f1a22590cf1af8db9412e9ce9af975
Varun <varunkverma1996@gmail.com>
2021-10-06 13:04:30 +0530

    repo 3

---
+++ webpack-tutorial-integrating-express-js-into-a-multiple-page-application/src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+// serving static files
+app.use("/static", express.static(path.resolve(__dirname, "../dist")));
+
+app.get("/hello-world", (req, res) => {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/hello-world.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contentFromHtmlFile);
+});
+app.get("/kiwi", (req, res) => {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/kiwi.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contentFromHtmlFile);
+});
+
+app.listen(3000, () => {
+  console.log("application is listening on port: 3000");
+});
